8b7abc53f88f6d53fad4c03754c41ffa99cb39ad
screwLock <helmlyw@gmail.com>
2018-06-12 01:54:31 -0400

    Need to add geocoder scripts

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -11,7 +11,14 @@ jQuery(document).ready(function($){
                      .column(2)
                      .search(15)
                      .draw();
-   
+
+         wpDataTables.table_1.DataTable()
+                     .column(13)
+                     .data()
+                     .each(function(value){
+                        codeAddress(value);
+                          });
+                     
          
         });
     });
@@ -22,9 +29,25 @@ jQuery(document).ready(function($){
 })(jQuery);
 
 var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('sapo_map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 11
-        });
-      }
+function initMap() {
+    map = new google.maps.Map(document.getElementById('sapo_map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 11
+    });
+}
+
+
+    function codeAddress(address) {
+       //var address = document.getElementById('address').value;
+       geocoder.geocode( { 'address': address}, function(results, status) {
+       if (status == google.maps.GeocoderStatus.OK) {
+          map.setCenter(results[0].geometry.location);
+          var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+       });
+       } else {
+          alert('Geocode was not successful for the following reason: ' + status);
+         }
+       });
+    }   
