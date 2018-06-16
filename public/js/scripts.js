73da258b40f99791c8bca0b16edb70728d55425d
screwLock <helmlyw@gmail.com>
2018-06-16 03:37:23 -0400

    Addded comments to functions.  Some cleaning up

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -37,9 +37,17 @@ jQuery(window).load(function(){
       });
 
 
-})        
+})        //END OF jQuery window load
     
+var markers = [];
 
+
+/**
+ * The callback needed for the google maps URL.  Specifies the map
+ * initial setting and zoom.
+ * 
+ *  
+ */
 function initMap() {
     map = new google.maps.Map(document.getElementById('sapo_map'), {
     center: {lat: 48.3552767, lng: -99.9995795},
@@ -48,6 +56,13 @@ function initMap() {
     });
     geocoder = new google.maps.Geocoder;    
 }
+
+/**
+ * The following three functions remove markers from the markers collection
+ * and reinitialize the array for new additions.
+ */
+
+ //////////////////////////////////////////////////////////////////
 
 function clearMarkers() {
     setMapOnAll(null);
@@ -64,8 +79,15 @@ function deleteMarkers() {
     }
   }
 
-var markers = [];
+///////////////////////////////////////////////////////////////////
 
+/**
+ * Converts a physical address into a marker with lat and long coordinates
+ * 
+ * 
+ * @param {*} address 
+ * 
+ */
 function codeAddress(address) {
    geocoder.geocode( { 'address': address}, function(results, status) {
    if (status == google.maps.GeocoderStatus.OK) {
@@ -82,7 +104,6 @@ function codeAddress(address) {
       });
 
        marker.addListener('click', addInfoWindow.bind(this, marker, infowindow, 15));
-      console.log("agin");
        markers.push(marker);
        } 
    else {
@@ -91,6 +112,16 @@ function codeAddress(address) {
    });
 }   
 
+
+/**
+ * Attach an infowindow to a marker with specified zoom settings
+ * 
+ * 
+ * @param {*} marker 
+ * @param {*} infowindow 
+ * @param {*} zoom 
+ * 
+ */
 function addInfoWindow(marker, infowindow, zoom){
     infowindow.open(map, marker);
     map.setZoom(zoom);
@@ -98,11 +129,28 @@ function addInfoWindow(marker, infowindow, zoom){
 }
 
 
+/**
+ * Delete and add new markers when table is redrawn
+ * 
+ * 
+ * @param {*} tableName 
+ * @param {*} column 
+ * 
+ */
 function updateMarkersOnRedraw(tableName, column){
   deleteMarkers();
   changeMarkers(tableName, column); 
 }
 
+
+/**
+ * Update the markers on the map by matches to a submitted address
+ *
+ * 
+ * @param {*} tableName 
+ * @param {*} addressColumn 
+ * 
+ */
 function changeMarkers(tableName, addressColumn){
   tableName.DataTable()
   .column(addressColumn)
@@ -112,6 +160,16 @@ function changeMarkers(tableName, addressColumn){
   });
 }
 
+
+/**
+ * Draw the table based on row matches to a submitted date
+ * 
+ * 
+ * @param {*} tableName 
+ * @param {*} dateColumn 
+ * @param {*} date 
+ * 
+ */
 function drawTableByDate(tableName, dateColumn, date){
   tableName.DataTable()
   .column(dateColumn)
